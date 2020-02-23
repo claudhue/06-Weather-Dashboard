96e8e1074050558592bd2ca44aa1937dd75fa8a9
Francisco Goya <degoyafran@gmail.com>
2020-02-23 15:25:35 -0600

    5 day forcast

--- script.js
+++ script.js
@@ -19,7 +19,16 @@ $(".city").text(response.city.name);
 $(".temp").text("Temperature: " + response.list[0].main.temp);
 $(".humid").text("Humidity: " + response.list[0].main.humidity);
 $(".ws").text("Wind Speed: " + response.list[0].wind.speed);
-$(".temp").text("Temperature: " + response.list[0].main.temp);
+$(".temp1").text("Temp: " + response.list[1].main.temp);
+$(".humid1").text("Humidity: " + response.list[1].main.humidity);
+$(".temp2").text("Temp: " + response.list[2].main.temp);
+$(".humid2").text("Humidity: " + response.list[2].main.humidity);
+$(".temp3").text("Temp: " + response.list[3].main.temp);
+$(".humid3").text("Humidity: " + response.list[3].main.humidity);
+$(".temp4").text("Temp: " + response.list[4].main.temp);
+$(".humid4").text("Humidity: " + response.list[4].main.humidity);
+$(".temp5").text("Temp: " + response.list[5].main.temp);
+$(".humid5").text("Humidity: " + response.list[5].main.humidity);
 
 });
 
